b837697721fc7b7b7e7966c87065df59a77360a5
Matthew Moseley <matthew.a.moseley@gmail.com>
2020-11-18 22:03:38 -0500

    Revert "Oops, we didn't want this commit"
    This reverts commit 5b617f686a924e39c1bb36dd12c9ae09d4d5641b.

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 public class Hello
-{ 
-	public static void main( String argv[] )
+{ public static void main( String argv[] )
 	{
-		// This is an unwanted but staged comment
 		String name = "World";
 		if ( argv.length != 0 )
 		{
